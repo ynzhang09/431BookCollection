5656558f3f9106e559099fe67ef4183d1d319843
Yuning Zhang <yuning.zhang09@gmail.com>
2022-09-15 21:00:44 -0500

    Final Commit

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,33 +2,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid title' do
+  scenario 'valid inputs - title' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
-    click_button 'Create Book'
+    click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
   end
-  scenario 'valid author' do
+  scenario 'valid inputs - author' do
     visit new_book_path
-    fill_in 'Author', with: 'Author name'
+    fill_in 'Author', with: 'author name'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('Author name')
+    expect(page).to have_content('author name')
   end
-  scenario 'valid price' do
+  scenario 'valid inputs - price' do
     visit new_book_path
-    fill_in 'Price', with: 20
+    fill_in 'Price', with: 10
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content(20)
+    expect(page).to have_content(10)
   end
-  scenario 'valid published_date' do
+  scenario 'valid inputs - published date' do
     visit new_book_path
-    fill_in 'Published_date', with: 01-05-2000
+    fill_in 'Published Date', with: 01-01-2000
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content(01-05-2000)
+    expect(page).to have_content(01-01-2000)
   end
-
 end
