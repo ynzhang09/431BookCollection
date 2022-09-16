06d93726bb8623d968ae60a50dad7b78827a81ed
Yuning Zhang <yuning.zhang09@gmail.com>
2022-09-15 19:32:50 -0500

    Implemented CRUD functionality

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid title' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
-    click_on 'Create Book'
+    click_button 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
   end
@@ -25,7 +25,7 @@ RSpec.describe 'Creating a book', type: :feature do
   end
   scenario 'valid published_date' do
     visit new_book_path
-    fill_in 'Published Date', with: 01-05-2000
+    fill_in 'Published_date', with: 01-05-2000
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content(01-05-2000)
